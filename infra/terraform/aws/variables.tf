9e32463458be53dbed54c3b5ea86037e6c80dcc7
Aatikah <133808155+Aatikah-Sheikh@users.noreply.github.com>
2025-11-15 11:58:53 +0200

    Update variables.tf

--- infra/terraform/aws/variables.tf
+++ infra/terraform/aws/variables.tf
@@ -1,20 +1,52 @@
-variable "region"      { type = string }
-variable "name"        { type = string }
-variable "vpc_id"      { type = string }
-variable "vpc_cidr"    { type = string }
-variable "subnet_id"   { type = string }
-
-variable "instance_type" {
-  type    = string
-  default = "t3.small"
+variable "region" {
+  type        = string
+  description = "AWS region, e.g. eu-north-1"
 }
 
-variable "ami_id"      { type = string }
-variable "ssh_cidr"    { type = string }   # YOUR.PUBLIC.IP/32
-variable "key_name"    { type = string }
-variable "public_key"  { type = string }
+variable "name" {
+  type        = string
+  description = "Short cluster/team name used for tags and prefixes"
+  default     = "wits-a"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "Target VPC ID (vpc-xxxxxxxx)"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR of the VPC (e.g. 172.31.0.0/16)"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID (subnet-xxxxxxxx) to place the instance in"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "Ubuntu AMI ID in your region (e.g. ami-xxxxxxxx)"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "ssh_cidr" {
+  type        = string
+  description = "Your public IP in CIDR form, e.g. 105.242.69.70/32"
+}
+
+variable "public_key" {
+  type        = string
+  description = "Your SSH public key (single line: ssh-ed25519 ...)"
+}
 
 variable "ssh_user" {
-  type    = string
-  default = "ubuntu"
+  type        = string
+  description = "Default SSH username for the AMI"
+  default     = "ubuntu"
 }
